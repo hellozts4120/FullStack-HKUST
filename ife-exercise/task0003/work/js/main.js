a0e9e9645fec2ba530d703e7b4ff1c8906e9aa26
hellozts4120 <14307110190@fudan.edu.cn>
2016-04-07 20:00:34 +0800

    roughly done but still have much to improve...

--- ife-exercise/task0003/work/js/main.js
+++ ife-exercise/task0003/work/js/main.js
@@ -5,6 +5,10 @@ localStorage.clear();
     var taskCount = 0;
     var _List1Title, _List2Title, _List3Content, _ListRelation;
     var pattern = document.getElementsByTagName("body")[0];
+    var dateTemplate = "<div id=\"childtask-{{date}}\"><div class=\"childtask-date\">{{date}}</div></div>";
+    var taskTemplate = "<div class=\"childtask\" id=\"{{ID3}}\">{{title}}</div>";
+    var template1 = "<div class=\"cata-item\" id=\"cata-item-{{id}}\"><img src=\"img/tasklist.png\">{{name}}<div class=\"cata-delete\"><img src=\"img/delete.png\"></div></div><div class=\"cata-tasklist\" id=\"cata-item-{{id}}-tasklist\"></div>";
+    var template2 = "<div class=\"cata-task\" id=\"task-{{parentID}}-{{taskID}}\"><img src=\"img/childtask.png\">{{content}}({{childtaskNum}})<div class=\"cata-delete\"><img src=\"img/delete.png\"></div></div>";
     
     //增加父级分类
     function addList1(name) {
@@ -105,8 +109,6 @@ localStorage.clear();
     
     function initCategory() {
         $("#cata-list").innerHTML = "";
-        var template1 = "<div class=\"cata-item\" id=\"cata-item-{{id}}\"><img src=\"img/tasklist.png\">{{name}}<div class=\"cata-delete\"><img src=\"img/delete.png\"></div></div><div class=\"cata-tasklist\" id=\"cata-item-{{id}}-tasklist\"></div>";
-        var template2 = "<div class=\"cata-task\" id=\"task-{{parentID}}-{{taskID}}\"><img src=\"img/childtask.png\">{{content}}({{childtaskNum}})<div class=\"cata-delete\"><img src=\"img/delete.png\"></div></div>";
         for (var listItem in _List1Title) {
             $("#cata-list").innerHTML += template1.replace(/{{name}}/g, _List1Title[listItem]["title"]).replace(/{{id}}/g, listItem);
             for (var taskItem in _ListRelation[listItem]["task"]) {
@@ -205,6 +207,50 @@ localStorage.clear();
         delegateEvent();
     }
     
+    function handleTaskHeader(tagChild, status) {
+        if ($(".task-header-clicked").length) {
+            $(".task-header-clicked")[0].setAttribute("class", "task-header-button");
+        }
+        addClass(tagChild, "task-header-clicked");
+        var taskArray = getChildTask($(".cata-task-clicked")[0].id).sort(sortToDoByDate);
+        var fliterdArray = [];
+        switch(status) {
+            case 1: 
+                fliterdArray = taskArray;
+                break;
+            case 2:
+                for (var cur in taskArray) {
+                    if (_List3Content[taskArray[cur]].status != undefined) {
+                        fliterdArray.push(taskArray[cur]);
+                    }
+                }
+                break;
+            case 3:
+                for (var cur in taskArray) {
+                    if (_List3Content[taskArray[cur]].status == undefined) {
+                        fliterdArray.push(taskArray[cur]);
+                    }
+                }
+                break;    
+        }
+        
+        var dateArray = [];
+        
+        for (var i = 0; i < fliterdArray.length; i++) {
+            dateArray.push(_List3Content[fliterdArray[i]].time);
+        }
+        if (dateArray.length) {
+            dateArray = uniqArray(dateArray);
+        }
+        $("#task-content").innerHTML = "";
+        for (var i = 0; i < dateArray.length; i++) {
+            $("#task-content").innerHTML += dateTemplate.replace(/{{date}}/g, dateArray[i]);
+        }
+        for (var i = 0; i < fliterdArray.length; i++) {
+            $("#childtask-" + _List3Content[fliterdArray[i]]["time"]).innerHTML += taskTemplate.replace(/{{ID3}}/g, "childtask-" + fliterdArray[i]).replace(/{{title}}/g, _List3Content[fliterdArray[i]]["title"]);
+        }
+    }
+    
     function delegateEvent() {
         
         pattern.onclick = function(e) {
@@ -275,36 +321,16 @@ localStorage.clear();
             
             //need debug here...
             if(tagChild.nodeType == 1 && tagChild.id == "task-header-all") {
-                console.log($(".cata-task-clicked")[0]);
-                if($(".task-header-clicked").length){
-                    $(".task-header-clicked")[0].setAttribute("class","task-header-button");
-                }
-                addClass(tagChild, "task-header-clicked");
-                var taskArray = getChildTask($(".cata-task-clicked")[0].id).sort(sortToDoByDate);
-                var dateArray = [];
-                
-                for (var i = 0; i < taskArray.length; i++) {
-                    if (dateArray.indexOf(_List3Content[taskArray[i]["time"]]) == -1) {
-                        dateArray.push(_List3Content[taskArray[i]].time);
-                    }
-                }
-                
-                $("#task-content").innerHTML = "";
-                for (var i = 0; i < dateArray.length; i++) {
-                    $("#task-content").innerHTML += dateTemplate.replace(/{{date}}/g, dateArray[i]);
-                }
-                for (var i = 0; i < taskArray.length; i++) {
-                    $("#childtask-" + _List3Content[taskArray[i]].time).innerHTML += taskTemplate.replace(/{{ID3}}/g, "childtask-" + taskArray[i]).replace(/{{title}}/g, _List3Content[taskArray[i]]["title"]);
-                }
+                handleTaskHeader(tagChild, 1);
                 
             }
             
-            if(tagChild.nodeType == 1 && tagChild.id=="task-header-done") {
-                
+            if(tagChild.nodeType == 1 && tagChild.id=="task-header-done") {            
+                handleTaskHeader(tagChild, 2);
             }
             
             if(tagChild.nodeType == 1 && tagChild.id=="task-header-undone") {
-                
+                handleTaskHeader(tagChild, 3);
             }
             
             if(tagChild.nodeType == 1 && tagChild.parentNode.id == "add-task") {
@@ -312,9 +338,6 @@ localStorage.clear();
                     if($(".cata-item-clicked")[0].id == "cata-item-1"){
                         alert("不能为默认分类添加子分类！");
                         return;
-                    }
-                    else {
-                        alert("输入有误，添加失败！")
                     }
                     $("#prompt2").setAttribute("style", "display:block;");
                     $("#prompt2").setAttribute("class", "cata-input");
@@ -325,6 +348,9 @@ localStorage.clear();
                 if ($("#prompt2").getAttribute("class") == "cata-input" && $("#prompt2-input").value) {
                     alert("添加子分类成功！");
                     addList2($("#prompt2-input").value, $(".cata-item-clicked")[0].id.split("-")[2]);
+                }
+                else {
+                    alert("输入有误，添加失败！")
                 }
                 $("#prompt2").removeAttribute("style");
                 $("#prompt2").removeAttribute("class");
@@ -355,7 +381,7 @@ localStorage.clear();
                 $("#content-header").innerHTML = _List3Content[id]["title"];
                 $("#content-date").innerHTML = _List3Content[id]["time"];
                 $("#content-body").innerHTML = _List3Content[id]["content"];
-                if (_List3Content[id].status != undefined && _List3Content[id].status == "completed") {
+                if ((_List3Content[id].status != undefined && _List3Content[id].status == "completed") || $("#childtask-title") == undefined) {
                     $("#taskComplete").setAttribute("style", "display:block;");
                     $("#taskSave").setAttribute("style", "display:none;");
                 }
@@ -375,7 +401,7 @@ localStorage.clear();
                     $("#taskSave").setAttribute("style", "display:block;");
                     $("#taskComplete").setAttribute("style", "display:none;");
                     var curTitle = $("#content-header").innerHTML;
-                    var cutDate = $("#content-date").innerHTML;
+                    var curDate = $("#content-date").innerHTML;
                     $("#content-header").innerHTML = "<input class=\"content-header-input\" id=\"childtask-title\"><p id=\"childtask-title-count\">还可输入12个字</p>";
                     $("#content-date").innerHTML = "<input class=\"content-header-input\" id=\"childtask-time\"><p id=\"childtask-time-alert\">请使用yyyy-mm-dd格式</p>";
                     $("#childtask-title").value = curTitle;
@@ -419,13 +445,15 @@ localStorage.clear();
                 }
             }
             
-            if(tagChild.parentNode.id == "taskComplete" && tagChild.parentNode.getAttribute("class").split(" ").length == 1 && $(".childtask-clicked").length){
+            //console.log(tagChild.parentNode);
+            if(tagChild.parentNode.id == "taskComplete" && $(".childtask-clicked").length){
                 if(confirm("确定任务完成？")){
                     var curItem = $(".childtask-clicked")[0];
                     var id = curItem.id.split("-")[1];
                     _List3Content[id].status = "complete";
                     localStorage.List3Content = JSON.stringify(_List3Content);
                     addClass($("#taskComplete"), "complete");
+                    alert("完成任务！");
                 }
             }
             
@@ -439,11 +467,19 @@ localStorage.clear();
                         return;
                     }
                     if (confirm("确认删除此分类吗？")) {
-                        tagChild.parentNode.parentNode.style.display = "none";
+                        var targetNode = tagChild.parentNode.parentNode;
+                        targetNode.style.display = "none";
+                        for (var cur = 0; cur < targetNode.childNodes.length; cur++) {
+                            targetNode.childNodes[cur].style.display = "none";
+                        }
+                        for (var cur in _ListRelation[ID2]["task"]) {
+                            delete _List2Title[cur];
+                        }
                         delete _ListRelation[ID2];
                         delete _List1Title[ID2];
                         localStorage.ListRelation = JSON.stringify(_ListRelation);
-                        localStorage.List2Title = JSON.stringify(_List1Title);
+                        localStorage.List1Title = JSON.stringify(_List1Title);
+                        localStorage.List2Title = JSON.stringify(_List2Title);
                     }
 
                 }
@@ -452,7 +488,7 @@ localStorage.clear();
                         alert("不能删除默认子分类！");
                         return;
                     }
-                    if (confirm("确认删除此分类吗？")) {
+                    if (confirm("确认删除此子分类吗？")) {
                         tagChild.parentNode.parentNode.style.display = "none";
                         delete _ListRelation[ID1]["task"][ID2];
                         delete _List2Title[ID2];
