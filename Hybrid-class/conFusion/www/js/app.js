3fb9c0e45f202ef388bfa47024a7f64ff38d8e12
hellozts4120 <14307110190@fudan.edu.cn>
2016-04-20 23:16:34 +0800

    hybrid ionic class week2

--- Hybrid-class/conFusion/www/js/app.js
+++ Hybrid-class/conFusion/www/js/app.js
@@ -79,6 +79,16 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
           controller: 'DishDetailController'
         }
       }
+    })
+    
+    .state('app.favorites', {
+      url: '/favorites',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/favorites.html',
+            controller:'FavoritesController'
+        }
+      }
     });
 
     // if none of the above states are matched, use this as the fallback
