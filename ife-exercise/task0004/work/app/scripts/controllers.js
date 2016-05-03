b29770231a7d2fc380997e4ef1a58607fd4816d3
hellozts4120 <14307110190@fudan.edu.cn>
2016-05-03 17:18:01 +0800

    modify

--- ife-exercise/task0004/work/app/scripts/controllers.js
+++ ife-exercise/task0004/work/app/scripts/controllers.js
@@ -47,5 +47,39 @@ angular.module('todo-mobile')
             }
         }
     }])
+    
+    .controller('CreateController', ['$scope', '$stateParams', 'cataFactory', 'taskFactory', function($scope, $stateParams, cataFactory, taskFactory) {
+        $scope.status = $stateParams.id;
+        $scope.content = {};
+        $scope.catagory = cataFactory.getCatagory();
+        for (var i in $scope.catagory) {
+            for (var j in $scope.catagory[i].child) {
+                if ($scope.catagory[i].child[j] == $stateParams.id) {
+                    $scope.fatherID = $scope.catagory[i].id;
+                }
+            }
+        }
 
+        if ($scope.status == 'new') {
+            $scope.content.title = '输入分类名';
+            $scope.content.id = parseInt(cataFactory.getCatagory().length);
+            $scope.content.child = [];
+        }
+        else {
+            $scope.content.title = '输入任务标题';
+            $scope.content.date = '输入任务时间';
+            $scope.content.content = '输入任务内容';
+            $scope.content.id = "t" + taskFactory.getTask().length;
+        }
+        
+        $scope.upload = function () {
+            if ($scope.status == 'new') {
+                cataFactory.upload($scope.content);
+            }
+            else {
+                cataFactory.uploadTask($scope.fatherID, $scope.content.id);
+                taskFactory.upload($scope.content);
+            }
+        }
+    }])
 ;
